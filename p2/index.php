bcd043d741f8fe7da0bee99e3268dbcbfe89e10c
Robert Holzhauser <RHolzhauser01@gmail.com>
2022-09-16 07:18:15 +0000

    practicing sub domains

--- p2/index.php
+++ p2/index.php
@@ -2,21 +2,21 @@
 <html lang='en'>
 
 <head>
-    <title>P1</title>
+    <title>p2</title>
     <meta charset='utf-8'>
 </head>
 
 <body>
-    <h1>Project 1</h1>
+    <h1>Project 2</h1>
 
     <img src='hes-logo.png'>
 
     <div style="background-color:crimson; color: white; text-align: center; font-size:x-large">
-        <strong>Project 1</strong>
+        <strong>Project 2</strong>
     </div>
 
     <?php
-		echo "Hello Project 1!";
+		echo "Hello Project 2";
 	?>
 </body>
 
--- p3/index.php
+++ p3/index.php
@@ -2,21 +2,21 @@
 <html lang='en'>
 
 <head>
-    <title>P1</title>
+    <title>p3</title>
     <meta charset='utf-8'>
 </head>
 
 <body>
-    <h1>Project 1</h1>
+    <h1>Project 3</h1>
 
     <img src='hes-logo.png'>
 
     <div style="background-color:crimson; color: white; text-align: center; font-size:x-large">
-        <strong>Project 1</strong>
+        <strong>PHP</strong>
     </div>
 
     <?php
-		echo "Hello Project 1!";
+		echo "Hello Project 3!";
 	?>
 </body>
 
