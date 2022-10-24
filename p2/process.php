e3a49ac8c5619f5afe8b0e7557819ddd4ea621a4
Robert Holzhauser <RHolzhauser01@gmail.com>
2022-10-24 02:39:19 +0000

    process.php receiving vars

--- p2/process.php
+++ p2/process.php
@@ -1,37 +1,37 @@
 <?php
 # first row
-$row0_col0 = $_POST['row0_col0'];
-$row0_col1 = $_POST['row0_col1'];
-$row0_col2 = $_POST['row0_col2'];
-$row0_col3 = $_POST['row0_col3'];
-$row0_col4 = $_POST['row0_col4'];
+$row0_col0 = $_POST['row0_col0'] > 0 ? $_POST['row0_col0'] : -1;
+$row0_col1 = $_POST['row0_col1'] > 0 ? $_POST['row0_col1'] : -1;
+$row0_col2 = $_POST['row0_col2'] > 0 ? $_POST['row0_col2'] : -1;
+$row0_col3 = $_POST['row0_col3'] > 0 ? $_POST['row0_col3'] : -1;
+$row0_col4 = $_POST['row0_col4'] > 0 ? $_POST['row0_col4'] : -1;
 
 #second row
-$row1_col0 = $_POST['row1_col0'];
-$row1_col1 = $_POST['row1_col1'];
-$row1_col2 = $_POST['row1_col2'];
-$row1_col3 = $_POST['row1_col3'];
-$row1_col4 = $_POST['row1_col4'];
+$row1_col0 = $_POST['row1_col0'] > 0 ? $_POST['row1_col0'] : -1;
+$row1_col1 = $_POST['row1_col1'] > 0 ? $_POST['row1_col1'] : -1;
+$row1_col2 = $_POST['row1_col2'] > 0 ? $_POST['row1_col2'] : -1;
+$row1_col3 = $_POST['row1_col3'] > 0 ? $_POST['row1_col3'] : -1;
+$row1_col4 = $_POST['row1_col4'] > 0 ? $_POST['row1_col4'] : -1;
 
 #third row
-$row2_col0 = $_POST['row2_col0'];
-$row2_col1 = $_POST['row2_col1'];
-$row2_col2 = $_POST['row2_col2'];
-$row2_col3 = $_POST['row2_col3'];
-$row2_col4 = $_POST['row2_col4'];
+$row2_col0 = $_POST['row2_col0'] > 0 ? $_POST['row2_col0'] : -1;
+$row2_col1 = $_POST['row2_col1'] > 0 ? $_POST['row2_col1'] : -1;
+$row2_col2 = $_POST['row2_col2'] > 0 ? $_POST['row2_col2'] : -1;
+$row2_col3 = $_POST['row2_col3'] > 0 ? $_POST['row2_col3'] : -1;
+$row2_col4 = $_POST['row2_col4'] > 0 ? $_POST['row2_col4'] : -1;
 
 #fourth row
-$row3_col0 = $_POST['row3_col0'];
-$row3_col1 = $_POST['row3_col1'];
-$row3_col2 = $_POST['row3_col2'];
-$row3_col3 = $_POST['row3_col3'];
-$row3_col4 = $_POST['row3_col4'];
+$row3_col0 = $_POST['row3_col0'] > 0 ? $_POST['row3_col0'] : -1;
+$row3_col1 = $_POST['row3_col1'] > 0 ? $_POST['row3_col1'] : -1;
+$row3_col2 = $_POST['row3_col2'] > 0 ? $_POST['row3_col2'] : -1;
+$row3_col3 = $_POST['row3_col3'] > 0 ? $_POST['row3_col3'] : -1;
+$row3_col4 = $_POST['row3_col4'] > 0 ? $_POST['row3_col4'] : -1;
 
 #fifth row
-$row4_col0 = $_POST['row4_col0'];
-$row4_col1 = $_POST['row4_col1'];
-$row4_col2 = $_POST['row4_col2'];
-$row4_col3 = $_POST['row4_col3'];
-$row4_col4 = $_POST['row4_col4'];
+$row4_col0 = $_POST['row4_col0'] > 0 ? $_POST['row4_col0'] : -1;
+$row4_col1 = $_POST['row4_col1'] > 0 ? $_POST['row4_col1'] : -1;
+$row4_col2 = $_POST['row4_col2'] > 0 ? $_POST['row4_col2'] : -1;
+$row4_col3 = $_POST['row4_col3'] > 0 ? $_POST['row4_col3'] : -1;
+$row4_col4 = $_POST['row4_col4'] > 0 ? $_POST['row4_col4'] : -1;
 
 require 'temp-process-view.php';
