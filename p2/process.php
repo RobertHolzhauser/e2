7f10079d43ee19ce47d6af0b3328aa86c33c4da9
Robert Holzhauser <RHolzhauser01@gmail.com>
2022-10-24 02:54:16 +0000

    temp working

--- p2/process.php
+++ p2/process.php
@@ -1,37 +1,36 @@
 <?php
 # first row
-$row0_col0 = $_POST['row0_col0'] > 0 ? $_POST['row0_col0'] : -1;
-$row0_col1 = $_POST['row0_col1'] > 0 ? $_POST['row0_col1'] : -1;
-$row0_col2 = $_POST['row0_col2'] > 0 ? $_POST['row0_col2'] : -1;
-$row0_col3 = $_POST['row0_col3'] > 0 ? $_POST['row0_col3'] : -1;
-$row0_col4 = $_POST['row0_col4'] > 0 ? $_POST['row0_col4'] : -1;
+$row0_col0 = key_exists('row0_col0', $_POST) ? $_POST['row0_col0'] : -1;
+$row0_col1 = key_exists('row0_col1', $_POST) ? $_POST['row0_col1'] : -1;
+$row0_col2 = key_exists('row0_col2', $_POST) ? $_POST['row0_col2'] : -1;
+$row0_col3 = key_exists('row0_col3', $_POST) ? $_POST['row0_col3'] : -1;
+$row0_col4 = key_exists('row0_col4', $_POST) ? $_POST['row0_col4'] : -1;
 
 #second row
-$row1_col0 = $_POST['row1_col0'] > 0 ? $_POST['row1_col0'] : -1;
-$row1_col1 = $_POST['row1_col1'] > 0 ? $_POST['row1_col1'] : -1;
-$row1_col2 = $_POST['row1_col2'] > 0 ? $_POST['row1_col2'] : -1;
-$row1_col3 = $_POST['row1_col3'] > 0 ? $_POST['row1_col3'] : -1;
-$row1_col4 = $_POST['row1_col4'] > 0 ? $_POST['row1_col4'] : -1;
+$row1_col0 = key_exists('row1_col0', $_POST) ? $_POST['row1_col0'] : -1;
+$row1_col1 = key_exists('row1_col1', $_POST) ? $_POST['row1_col1'] : -1;
+$row1_col2 = key_exists('row1_col2', $_POST) ? $_POST['row1_col2'] : -1;
+$row1_col3 = key_exists('row1_col3', $_POST) ? $_POST['row1_col3'] : -1;
+$row1_col4 = key_exists('row1_col4', $_POST) ? $_POST['row1_col4'] : -1;
 
 #third row
-$row2_col0 = $_POST['row2_col0'] > 0 ? $_POST['row2_col0'] : -1;
-$row2_col1 = $_POST['row2_col1'] > 0 ? $_POST['row2_col1'] : -1;
-$row2_col2 = $_POST['row2_col2'] > 0 ? $_POST['row2_col2'] : -1;
-$row2_col3 = $_POST['row2_col3'] > 0 ? $_POST['row2_col3'] : -1;
-$row2_col4 = $_POST['row2_col4'] > 0 ? $_POST['row2_col4'] : -1;
-
+$row2_col0 = key_exists('row2_col0', $_POST) ? $_POST['row2_col0'] : -1;
+$row2_col1 = key_exists('row2_col1', $_POST) ? $_POST['row2_col1'] : -1;
+$row2_col2 = key_exists('row2_col2', $_POST) ? $_POST['row2_col2'] : -1;
+$row2_col3 = key_exists('row2_col3', $_POST) ? $_POST['row2_col3'] : -1;
+$row2_col4 = key_exists('row2_col4', $_POST) ? $_POST['row2_col4'] : -1;
 #fourth row
-$row3_col0 = $_POST['row3_col0'] > 0 ? $_POST['row3_col0'] : -1;
-$row3_col1 = $_POST['row3_col1'] > 0 ? $_POST['row3_col1'] : -1;
-$row3_col2 = $_POST['row3_col2'] > 0 ? $_POST['row3_col2'] : -1;
-$row3_col3 = $_POST['row3_col3'] > 0 ? $_POST['row3_col3'] : -1;
-$row3_col4 = $_POST['row3_col4'] > 0 ? $_POST['row3_col4'] : -1;
+$row3_col0 = key_exists('row3_col0', $_POST) ? $_POST['row3_col0'] : -1;
+$row3_col1 = key_exists('row3_col1', $_POST) ? $_POST['row3_col1'] : -1;
+$row3_col2 = key_exists('row3_col2', $_POST) ? $_POST['row3_col2'] : -1;
+$row3_col3 = key_exists('row3_col3', $_POST) ? $_POST['row3_col3'] : -1;
+$row3_col4 = key_exists('row3_col4', $_POST) ? $_POST['row3_col4'] : -1;
 
 #fifth row
-$row4_col0 = $_POST['row4_col0'] > 0 ? $_POST['row4_col0'] : -1;
-$row4_col1 = $_POST['row4_col1'] > 0 ? $_POST['row4_col1'] : -1;
-$row4_col2 = $_POST['row4_col2'] > 0 ? $_POST['row4_col2'] : -1;
-$row4_col3 = $_POST['row4_col3'] > 0 ? $_POST['row4_col3'] : -1;
-$row4_col4 = $_POST['row4_col4'] > 0 ? $_POST['row4_col4'] : -1;
+$row4_col0 = key_exists('row4_col0', $_POST) ? $_POST['row4_col0'] : -1;
+$row4_col1 = key_exists('row4_col1', $_POST) ? $_POST['row4_col1'] : -1;
+$row4_col2 = key_exists('row4_col2', $_POST) ? $_POST['row4_col2'] : -1;
+$row4_col3 = key_exists('row4_col3', $_POST) ? $_POST['row4_col3'] : -1;
+$row4_col4 = key_exists('row4_col4', $_POST) ? $_POST['row4_col4'] : -1;
 
 require 'temp-process-view.php';
--- p2/temp-process-view.php
+++ p2/temp-process-view.php
@@ -1,2 +1,31 @@
 <?php
 echo $row0_col0;
+echo $row0_col1;
+echo $row0_col2;
+echo $row0_col3;
+echo $row0_col4;
+
+# row 2
+echo $row1_col0;
+echo $row1_col1;
+echo $row1_col2;
+echo $row1_col3;
+echo $row1_col4;
+
+echo $row2_col0;
+echo $row2_col1;
+echo $row2_col2;
+echo $row2_col3;
+echo $row2_col4;
+
+echo $row3_col0;
+echo $row3_col1;
+echo $row3_col2;
+echo $row3_col3;
+echo $row3_col4;
+
+echo $row4_col0;
+echo $row4_col1;
+echo $row4_col2;
+echo $row4_col3;
+echo $row4_col4;
